04857ba71eb1b4687335e96bc681b6e34b9ce6e1
Wilber Carrascal <wilber.carrascal@gmail.com>
2026-05-08 04:38:00 -0500

    fix: serve admin static assets in production

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,3 +4,12 @@ from .base import *
 
 DEBUG = False
 CORS_ALLOWED_ORIGINS = config("CORS_ALLOWED_ORIGINS", default="").split(",")
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
